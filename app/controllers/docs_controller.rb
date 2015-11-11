ca13ecb57daed5e88118906e86a2efc8ab01bef3
telwell <me@trevorelwell.com>
2015-11-11 08:29:49 -0500

    merge

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -21,11 +21,22 @@ class DocsController < ApplicationController
 		@quora_links = []
 		# Our Google Spreadsheet Links Begin at cell C10 and continue down
 		col = 3
-		for row in 10..spreadsheet.num_rows
-			@quora_links.push(spreadsheet[row, col])
-			puts spreadsheet[row, col]
+		for row in 10..spreadsheet.num_rows-1
+			quora_link = spreadsheet[row, col]
+			unless quora_link.empty?
+				mechanize_info = quora_mechanize(quora_link)
+				mechanize_info[:link] = quora_link
+				@quora_links.push(mechanize_info)
+			end
 		end
 		
 	end
 
+private
+	def quora_mechanize(link)
+		agent = Mechanize.new
+		agent.user_agent_alias = 'Mac Safari'
+		page = agent.get(link)
+		{ answer_count: page.search('.answer_count').inner_text, views: page.search('.ViewsRow').inner_text, followers: page.search('.QuestionFollowerListModalLink').inner_text }
+	end
 end
